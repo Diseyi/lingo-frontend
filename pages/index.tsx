c0487907c5f0fc4977afb6c22d4c4c2a7c4e0b97
diseyi <filomiinaa@gmail.com>
2022-08-15 11:38:02 +0100

    initial setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,17 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Lingo</title>
-        <meta name="description" content="chat app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="text-3xl font-bold text-center">Lingo</h1>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Lingo</title>
+                <meta name="description" content="chat app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1 className="text-3xl font-bold text-center">Lingo</h1>
       
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Home;
