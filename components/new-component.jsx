89427ac849cd4d73653ae24cbc7110376f41a8ba
Diseyi <filomiinaa@gmail.com>
2023-10-11 10:41:35 +0100

    update

--- components/new-component.jsx
+++
@@ -1,7 +0,0 @@
-export const MyComponent = () => {
-  return (
-    <button className="text-white font-semibold hover:bg-[#AAE8DF] hover:text-gray-600 px-8 py-2 rounded text-base boxshadow2 bg-[#52B1A4] cursor-pointer">
-      Sign up
-    </button>
-  );
-};
